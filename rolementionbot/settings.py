8b0b87edf2b7659dcfcf7823091f59f7ed88d04c
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2022-03-19 23:22:49 +0330

    Add support for webhook

---
+++ rolementionbot/settings.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+PREFIX = os.getenv("PREFIX", ";")
+BATCH = int(os.getenv("BATCH", 7))
+MAX_ROLES = int(os.getenv("MAX_ROLES", 10))
+ROLE_PATTERN = re.compile(r"(\s|^)@([a-zA-Z0-9_]{5,32})")
+IGNORE_STATUS = (telegram.ChatMember.LEFT,
+                 telegram.ChatMember.KICKED,
+                 telegram.ChatMember.RESTRICTED)
+ADMIN_STATUS = (telegram.ChatMember.ADMINISTRATOR,
+                telegram.ChatMember.CREATOR)
+
+TOKEN = os.getenv("TOKEN")
+if not TOKEN:
+    raise Exception("TOKEN not found")
+
+DB_FILE = os.getenv("DBFILE", "db/role.db")
+DB = Database(DB_FILE)
+
+REGISTERED = os.getenv("REGISTERED")
+if REGISTERED is None:
+    raise Exception("No registered groups")
+REGISTERED = set(map(int, REGISTERED.split(':')))
+
+DEBUG = bool(os.getenv("DEBUG"))
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+CERT_FILEPATH = os.getenv("CERT_FILEPATH")
+PORT = os.getenv("PORT")
+if PORT is not None:
+    PORT = int(PORT)
